317d31b00ff7bd4dc580b6d0db458ee4cfc06810
abdiqafar-ai <iabdiqafar24@gmail.com>
2024-09-19 23:33:29 +0300

    first commit

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-// Code your solutions in this file
+const thankYouCards=[]
+function writeCards(array, event) {
+    for(let i =0; i < array.length; i++){
+      thankYouCards.push(`Thank you, ${array[i]}, for the wonderful ${event} gift!`);
+    }; return thankYouCards;
+  };
+  function countDown(number){
+    while (number >= 0) {
+      console.log(number--);}}
